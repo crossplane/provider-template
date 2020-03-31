bb02854b9a33ef5f8d1acdbf0ee15c68b7919825
hasheddan <georgedanielmangum@gmail.com>
2020-03-31 08:27:49 -0500

    Initial project scaffolding
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ apis/v1alpha1/types.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	runtimev1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
+)
+
+// A ProviderSpec defines the desired state of a Provider.
+type ProviderSpec struct {
+	runtimev1alpha1.ProviderSpec `json:",inline"`
+
+	// Add any other fields here for information that is specific to authenticating with an account for the provider
+}
+
+// +kubebuilder:object:root=true
+
+// A Provider configures a Template 'provider'
+// +kubebuilder:printcolumn:name="PROJECT-ID",type="string",JSONPath=".spec.projectID"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:printcolumn:name="SECRET-NAME",type="string",JSONPath=".spec.credentialsSecretRef.name",priority=1
+// +kubebuilder:resource:scope=Cluster
+type Provider struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ProviderSpec `json:"spec"`
+}
+
+// +kubebuilder:object:root=true
+
+// ProviderList contains a list of Provider
+type ProviderList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Provider `json:"items"`
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/crossplane-book/provider-template
+
+go 1.13
+
+require (
+	github.com/crossplane/crossplane v0.9.0
+	github.com/crossplane/crossplane-runtime v0.6.0
+	github.com/crossplane/crossplane-tools v0.0.0-20200303232609-b3831cbb446d
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	k8s.io/apimachinery v0.17.3
+	sigs.k8s.io/controller-runtime v0.4.0
+	sigs.k8s.io/controller-tools v0.2.4
+)
