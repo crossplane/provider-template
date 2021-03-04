0b4223dd43cd657a92e79f37981cc1a799d80ae9
Nic Cope <nicc@rk0n.org>
2021-03-04 12:25:50 -0800

    Fix typo in leader-election help
    Signed-off-by: Nic Cope <negz@github.com>

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -35,7 +35,7 @@ func main() {
 		app            = kingpin.New(filepath.Base(os.Args[0]), "Template support for Crossplane.").DefaultEnvars()
 		debug          = app.Flag("debug", "Run with debug logging.").Short('d').Bool()
 		syncPeriod     = app.Flag("sync", "Controller manager sync period such as 300ms, 1.5h, or 2h45m").Short('s').Default("1h").Duration()
-		leaderElection = app.Flag("leader-election", "Use leader election for the conroller manager.").Short('l').Default("false").OverrideDefaultFromEnvar("LEADER_ELECTION").Bool()
+		leaderElection = app.Flag("leader-election", "Use leader election for the controller manager.").Short('l').Default("false").OverrideDefaultFromEnvar("LEADER_ELECTION").Bool()
 	)
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
