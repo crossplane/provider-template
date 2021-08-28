7230f3ccf76669ddf0973fedb66ab7ad8597eb69
Muvaffak Onus <me@muvaf.com>
2021-08-28 16:10:24 +0300

    Do not use PROVIDER-NAME as placeholder for CRD category because CRDs generated with that fail to register to apiserver
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- apis/sample/v1alpha1/mytype_types.go
+++ apis/sample/v1alpha1/mytype_types.go
@@ -54,8 +54,7 @@ type MyTypeStatus struct {
 // +kubebuilder:printcolumn:name="STATUS",type="string",JSONPath=".status.bindingPhase"
 // +kubebuilder:printcolumn:name="STATE",type="string",JSONPath=".status.atProvider.state"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
-// Please replace `PROVIDER-NAME` with your actual provider name, like `aws`, `azure`, `gcp`, `alibaba`
-// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,PROVIDER-NAME}
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,template}
 type MyType struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
