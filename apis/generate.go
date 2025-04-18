a96f2bb45cc87baad421367c1050b2d1e05fd011
Chuan-Yen Chiang <cychiang0823@gmail.com>
2025-04-19 07:19:35 +0800

    * Update Copyright to 2025 * Update templates
    Signed-off-by: Chuan-Yen Chiang <cychiang0823@gmail.com>

--- apis/generate.go
+++ apis/generate.go
@@ -1,7 +1,8 @@
+//go:build generate
 // +build generate
 
 /*
-Copyright 2020 The Crossplane Authors.
+Copyright 2025 The Crossplane Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
